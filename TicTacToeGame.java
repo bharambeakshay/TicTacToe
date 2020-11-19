ef349dbb968754f15d6ecc92a61dcbfd00b1aa6b
akshay <bharambeakshay1997@gmail.com>
2020-11-19 11:45:42 +0530

    Symbol selection is done

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
+import java.net.StandardSocketOptions;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToeGame {
     private static ArrayList<ArrayList<String> > board;
@@ -13,11 +15,28 @@ public class TicTacToeGame {
         board.add( new ArrayList<String>(Arrays.asList(" "," "," ")));
         board.add( new ArrayList<String>(Arrays.asList(" "," "," ")));
         }
+    //Symbol Selection
+    public static void chooseSymbol(int userSymbolChoice){
+        if (userSymbolChoice == 1){
+            playerSymbol = "X";
+            cpuSymbol  =  "O";
+        }
+        else{
+            playerSymbol = "O";
+            cpuSymbol = "X";
+        }
+
+    }
+
 
 
     public static void main(String args[]){
         TicTacToeGame tictactoe = new TicTacToeGame();
         tictactoe.initBoard();
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Enter the Symbol Choice ");
+        int symChoice = choice.nextInt();
+        tictactoe.chooseSymbol(symChoice);
 
 
     }
